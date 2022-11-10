33a301b29e9a4b4f364779b137b44ba64cc31f48
Felipe Rodrigues <lfrs.web@gmail.com>
2022-11-11 00:10:54 +0100

    Set address.number and address.postal_code as nullable

--- src/main/kotlin/io/billie/organisations/viewmodel/AddressRequest.kt
+++ src/main/kotlin/io/billie/organisations/viewmodel/AddressRequest.kt
@@ -6,8 +6,8 @@ import javax.validation.constraints.NotBlank
 
 data class AddressRequest(
     @field:NotBlank val street: String,
-    val number: Int,
-    @JsonProperty("postal_code") val postalCode: Int,
+    val number: Int?,
+    @JsonProperty("postal_code") val postalCode: Int?,
     @field:NotBlank @JsonProperty("country_code") val countryCode: String,
     @field:NotBlank @JsonProperty("city_id") val cityId: String,
 )
