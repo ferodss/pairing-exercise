49c3d1d9d2f810b9a855d6a72af17297d955aef8
Felipe Rodrigues <lfrs.web@gmail.com>
2022-11-10 22:32:32 +0100

    Org address test dont need to mock org

--- src/test/kotlin/io/billie/functional/CanStoreAndReadOrganisationAddressTest.kt
+++ src/test/kotlin/io/billie/functional/CanStoreAndReadOrganisationAddressTest.kt
@@ -1,9 +1,5 @@
 package io.billie.functional
 
-import com.ninjasquad.springmockk.MockkBean
-import io.billie.organisations.viewmodel.Entity
-import io.billie.organisations.service.OrganisationService
-import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -12,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDO
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import java.util.*
 
@@ -26,14 +22,9 @@ class CanStoreAndReadOrganisationAddressTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @MockkBean
-    private lateinit var organisationService: OrganisationService
-
     @Test
     fun orgHasNoAddressByDefault() {
         val orgId = UUID.randomUUID()
-
-        every { organisationService.findOrganisationById(orgId) } returns Entity(orgId)
 
         mockMvc.perform(
             get("/organisations/$orgId/address").contentType(APPLICATION_JSON)
