cb9574170d67204f60d9dde03248c0bea8cf41b3
Felipe Rodrigues <lfrs.web@gmail.com>
2022-11-15 16:00:40 +0100

    Set address.postal_code as nullable string instead of nullable int

--- src/main/kotlin/io/billie/organisations/viewmodel/AddressRequest.kt
+++ src/main/kotlin/io/billie/organisations/viewmodel/AddressRequest.kt
@@ -1,13 +1,12 @@
 package io.billie.organisations.viewmodel
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.util.UUID
 import javax.validation.constraints.NotBlank
 
 data class AddressRequest(
     @field:NotBlank val street: String,
     val number: Int?,
-    @JsonProperty("postal_code") val postalCode: Int?,
+    @JsonProperty("postal_code") val postalCode: String?,
     @field:NotBlank @JsonProperty("country_code") val countryCode: String,
     @field:NotBlank @JsonProperty("city_id") val cityId: String,
 )
